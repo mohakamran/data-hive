3d0d46a1768b8835c1b1eab18cc9977c17051ea3
Muhammad Kamran <51443339+mohakamran@users.noreply.github.com>
2026-04-02 19:37:01 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/data-hive/',
+  base: '/',
   build: {
     outDir: 'dist',
   },
